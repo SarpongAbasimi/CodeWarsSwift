08fd9cc11e02e9a663b0d4f3381ef08da662086e
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-02-08 16:29:24 +0000

    Update SwitchitUp(8kyu).swift

--- SwitchitUp(8kyu).swift
+++ SwitchitUp(8kyu).swift
@@ -29,3 +29,6 @@ func switchItUp(_ number: Int) -> String {
         return "Zero"
     }
 }
+
+let myobject:[Int:String] = [0:"Zero",1:"One",2:"Two",3:"Three",4:"Four",5:"Five",6:"Six",7:"Seven",8:"Eight",9:"Nine"];
+return myobject[number]!
