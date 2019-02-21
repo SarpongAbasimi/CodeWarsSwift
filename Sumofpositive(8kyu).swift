e1e8aa7443f3690d9f486de8dbb9230af440ccee
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-02-21 12:52:13 +0000

    Update Sumofpositive(8kyu).swift

--- Sumofpositive(8kyu).swift
+++ Sumofpositive(8kyu).swift
@@ -1,6 +1,6 @@
 
 //You get an array of numbers, return the sum of all of the positives ones.
-//Example [1,-4,7,12] => 1 + 7 + 12 = 20
+//Example [1,-4,7,12] => 1 + 7 + 12 = 20.
 
 func sumOfPositives (_ numbers: [Int] ) -> Int {
     // Your code here
